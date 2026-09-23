304a8d93aef4059b9191af69f2a046b31d3be770
whotterre <ijeddy5.0@gmail.com>
2026-09-24 00:52:03 +0100

    feat: add portion to check OS is supported

--- mole.sh
+++ mole.sh
@@ -1,9 +1,24 @@
 #!/usr/bin/bash
 echo "Prepping to steal MovieBox files..."
 
-set -eux pipefail
+set -euxo pipefail
 
 MOVIES_DIR=/storage/emulated/0/Android/data/com.community.oneroom/files/Download/d
 SUBS_DIR=/storage/emulated/0/Android/data/com.community.oneroom/files/Download/subtitle
 
-# Check OS
+# Check OS 
+case "$OSTYPE" in
+    linux*)
+        echo "OS detected: Linux"
+        ;;
+    darwin*)
+        echo "OS detected: macOS"
+        ;;
+    msys* | cygwin* | mingw*)
+        echo "OS detected: Windows (Bash Environment)"
+        ;;
+    *)
+        echo "Error: Operating system '$OSTYPE' is not supported by this script." >&2
+        exit 1
+        ;;
+esac 
